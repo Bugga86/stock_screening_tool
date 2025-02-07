1a0fbfca92dee97d9668a2d2f2c01c5b26163b27
Rohit Parab <rohitparab@Rohits-MacBook-Air.local>
2025-02-08 00:40:02 +0530

    Chore: adding the function for final score

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ fn collect_and_sort<F>(
     records: &[IssuerDetail],
     key_extractor: F,
     descending: bool,
-) -> Vec<(String, f64)>
+) -> Vec<(String, i32)>
 where
     F: Fn(&IssuerDetail) -> f64
 {
@@ -44,7 +44,7 @@ where
     } else {
         results.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap_or(std::cmp::Ordering::Equal));
     }
-    results
+    generate_score_for_factor(results.clone())
 }
 
 fn generate_score_for_factor(
@@ -59,90 +59,57 @@ fn generate_score_for_factor(
         }).collect()
 }
 
-#[allow(unused)]
 fn run() -> Result<(), Box<dyn Error>> {
     // let mut records = Vec::new();
     let mut rdr = csv::Reader::from_path("resources/Bank_details.csv")?;
-    let mut records: Vec<IssuerDetail> = rdr.deserialize()
+    let records: Vec<IssuerDetail> = rdr.deserialize()
         .map(|result| result.expect("Failed to deserialize record"))
         .collect();
 
-    let cmp_vec = collect_and_sort(&records, |r| r.CMP, false);
-    let pe_vec = collect_and_sort(&records, |r| r.PE, false);
-    let market_cap = collect_and_sort(&records, |r| r.Mar_Cap, true);
-    let div_yield_vec = collect_and_sort(&records, |r| r.Div_Yld, true);
-    let np_qtr_vec = collect_and_sort(&records, |r| r.NP_Qtr, true);
-    let sales_qtr_vec = collect_and_sort(&records,|r| r.Sales_Qtr, true);
-    let roce = collect_and_sort(&records, |r| r.ROCE,true);
-    let sales_var_5yrs_vec = collect_and_sort(&records, |r| r.Sales_Var_5Yrs, true);
-    let debt_per_equity = collect_and_sort(&records,|r| r.Debt_per_Eq, false);
-    let roe = collect_and_sort(&records,|r|r.ROE,true);
-    let eps_12m_vec = collect_and_sort(&records,|r|r.EPS_12M, true);
-    let profit_growth_vec = collect_and_sort(&records, |r| r.Profit_growth, true);
-    let opm = collect_and_sort(&records,|r| r.OPM,true);
-    let opm_5years = collect_and_sort(&records,|r|r.OPM_5yrs,true);
-    let piotski_scr_vec = collect_and_sort(&records, |r| r.Piotski_Scr as f64, true);
+    let cmp_vec_score = collect_and_sort(&records, |r| r.CMP, false);
+    let pe_vec_score = collect_and_sort(&records, |r| r.PE, false);
+    let market_cap_score = collect_and_sort(&records, |r| r.Mar_Cap, true);
+    let div_yield_score = collect_and_sort(&records, |r| r.Div_Yld, true);
+    let np_qtr_score = collect_and_sort(&records, |r| r.NP_Qtr, true);
+    let sales_qtr_score = collect_and_sort(&records,|r| r.Sales_Qtr, true);
+    let roce_score = collect_and_sort(&records, |r| r.ROCE,true);
+    let sales_var_5yrs_score = collect_and_sort(&records, |r| r.Sales_Var_5Yrs, true);
+    let debt_per_equity_score = collect_and_sort(&records,|r| r.Debt_per_Eq, false);
+    let roe_score = collect_and_sort(&records,|r|r.ROE,true);
+    let eps_12m_score = collect_and_sort(&records,|r|r.EPS_12M, true);
+    let profit_growth_score = collect_and_sort(&records, |r| r.Profit_growth, true);
+    let opm_score = collect_and_sort(&records,|r| r.OPM,true);
+    let opm_5years_score = collect_and_sort(&records,|r|r.OPM_5yrs,true);
+    let piotski_scr_score = collect_and_sort(&records, |r| r.Piotski_Scr as f64, true);
 
-    // let cmp_vec_score: Vec<(String, i32)> = cmp_vec
-    //     .iter()
-    //     .enumerate()
-    //     .map(|(index, item)| {
-    //         (item.0.clone(), (10 - index as i32))
-    //     })
-    //     .collect();
-    // print!("{:?}",cmp_vec_score);
+    let mut final_score_map: std::collections::HashMap<String, i32> = std::collections::HashMap::new();
 
-    let cmp_vec_score: Vec<(String, i32)> = generate_score_for_factor(cmp_vec);
+    let all_scores = vec![
+        cmp_vec_score,
+        pe_vec_score,
+        market_cap_score,
+        div_yield_score,
+        np_qtr_score,
+        sales_qtr_score,
+        roce_score,
+        sales_var_5yrs_score,
+        debt_per_equity_score,
+        roe_score,
+        eps_12m_score,
+        profit_growth_score,
+        opm_score,
+        opm_5years_score,
+        piotski_scr_score,
+    ];
 
-    println!("{:?}",cmp_vec_score);
-    let pe_vec_score: Vec<(String, i32)> = pe_vec
-        .iter()
-        .enumerate()
-        .map(|(index, item)| {
-            (item.0.clone(),(10 - index as i32))
-        })
-        .collect();
-
-    println!("{:?}",pe_vec_score);
-
-    let final_score_vec: Vec<(String, i32)> = cmp_vec_score
-        .iter()
-        .filter_map(|(issuer_name, issuer_score)| {
-            pe_vec_score.iter().find_map(|(issuer_name_2, score_2)| {
-                if issuer_name == issuer_name_2 {
-                    Some((issuer_name.clone(), issuer_score + score_2))
-                } else {
-                    None
-                }
-            })
-        })
-        .collect();
-
+    for scores in all_scores {
+        for (name, score) in scores {
+            *final_score_map.entry(name).or_insert(0) += score;
+        }
+    }
+    let mut final_score_vec: Vec<(String, i32)> = final_score_map.into_iter().collect();
+    final_score_vec.sort_by(|a,b| b.1.partial_cmp(&a.1).unwrap_or(std::cmp::Ordering::Equal));
     println!("{:?}",final_score_vec);
-    // .filter_map(|(index, item)| {
-    //     if market_cap.iter().any(|issuer| issuer.0 == item.0) {
-    //         Some((item.0.clone(), index))
-    //     } else {
-    //         None
-    //     }
-    // })
-    // .collect();
-    // println!("{:?}", pe_vec);
-    // println!("{:?}", market_cap);
-    // println!("{:?}", div_yield_vec);
-    // println!("{:?}", np_qtr_vec);
-    // println!("{:?}", sales_qtr_vec);
-    // println!("{:?}", roce);
-    // println!("{:?}", sales_var_5yrs_vec);
-    // println!("{:?}", debt_per_equity);
-    // println!("{:?}", roe);
-    // println!("{:?}", eps_12m_vec);
-    // println!("{:?}", profit_growth_vec);
-    // println!("{:?}", opm);
-    // println!("{:?}", opm_5years);
-    // println!("{:?}", piotski_scr_vec);
-
-
     Ok(())
 }
 
